18263108674b4ecbfc1b2832121fbdecdd3f26b0
RachaelBamigboye <105458643+RachaelBamigboye@users.noreply.github.com>
2024-02-26 16:37:05 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,17 @@
+// let key=42;
+
+// function getMessage(value){
+
+//     let keyGenerator=function(){
+//         let key=12;
+//         console.log('in keyGenerator:', key);
+//         return key;
+//     }
+//    let code=value*keyGenerator();
+//    console.log('in getsecretcode:', key);
+//    return code;
+// }
+// let secretCode=getMessage(2);
+// showMessage(secretCode);
+
+percentOff(45);
---
+++ utils.js
@@ -0,0 +1,7 @@
+function showMessage(message){
+    document.getElementById('message').textContent=message;
+    
+}
+let percentOff=function(percentage){
+    document.getElementById('percent-off').textContent=percentage+'% OFF';
+}
